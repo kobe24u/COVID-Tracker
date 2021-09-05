48a500c593b7531f78b44c427c6b71437a3fc874
Vinnie Liu <vinnie.liu@rea-group.com>
2021-09-05 18:41:40 +1000

    Initial Commit

---
+++ COVID Tracker VIC/COVID_Tracker_VICApp.swift
@@ -0,0 +1,17 @@
+//
+//  COVID_Tracker_VICApp.swift
+//  COVID Tracker VIC
+//
+//  Created by Vinnie Liu on 5/9/21.
+//
+
+import SwiftUI
+
+@main
+struct COVID_Tracker_VICApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
